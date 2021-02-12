26327d86a81225e9069c93df623b395e5f520721
Iryna Cherpak <cherpak.iryna@gmail.com>
2021-02-12 11:05:24 +0200

    First page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Phones App</h1>
       </header>
+
+      <div class="container-fluid">
+        <div data-component="App">
+          <h3>Content will be here</h3>
+        </div>
+      </div>
+
+      <footer>Footer</footer>
     </div>
   );
 }
